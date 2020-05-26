68b0e8cf0e841a1756edc34548f8f36561c47b53
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-05-26 14:29:33 -0700

    first commit

--- pong.py
+++ pong.py
@@ -1,1 +1,13 @@
 # Simple Pomg in Python 3
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by @carvalhojg")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Main game loop
+while True:
+    wn.update()
